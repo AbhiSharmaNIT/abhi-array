785ee15df2f6264104f3ca9b074aac52aa6476df
Abhishek Kumar <136017324+AbhiSharmaNIT@users.noreply.github.com>
2023-10-18 21:10:12 +0530

    Add files via upload

---
+++ array1.cpp
@@ -0,0 +1,12 @@
+// Ques.-How to make a program form array for finding any number are exist in array?
+#include <iostream>
+using namespace std;
+
+int main()
+{
+
+    int array[4] = {23, 34, 45, 56};
+    cout << array[2] << endl;
+
+    return 0;
+}
