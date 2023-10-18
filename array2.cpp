d4ac330e21ded62ec0c29079e2346a472cb74aca
Abhishek Kumar <136017324+AbhiSharmaNIT@users.noreply.github.com>
2023-10-18 21:11:32 +0530

    Add files via upload

---
+++ array2.cpp
@@ -0,0 +1,21 @@
+//Ques.- How to declear array in the program "basic" ?
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int n;
+    cin>>n;
+
+    int arr[n];
+    for (int i = 0; i < n; i++)
+    {
+        cin>>arr[i];
+    }
+    
+    for (int i = 0; i < n; i++)
+    {
+        cout<<arr[i]<<" ";
+    }
+   return 0; 
+}
